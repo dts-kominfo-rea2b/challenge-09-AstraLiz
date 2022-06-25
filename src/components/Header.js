3c0767e06f715a49e0dec9686874d17a37f1ac1b
Geovanni Alvin <geovannialvinprasetya@yahoo.com>
2022-06-25 15:59:47 +0900

    React Challenge: Intro

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-// Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+// Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css'
 
 const Header = () => {
     return (
-        <></>
+        <div className='head'>
+            <h1 className='title'>Call A Friend</h1>
+            <h3 className='sub-title'>"your friendly contact app"</h3>
+            <p class='dots'>. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . </p>
+        </div>
     )
 }
 
